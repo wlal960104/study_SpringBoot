896e49ad362ad20dd8e7e8f2263ac10fe34561a9
jimi <jimi96@softworks.co.kr>
2024-08-14 16:46:50 +0900

    [24.08.14] 5강 숙제 완료

--- src/main/java/com/apple/shop/BasicController.java
+++ src/main/java/com/apple/shop/BasicController.java
@@ -3,6 +3,8 @@ package com.apple.shop;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 @Controller
 public class BasicController {
@@ -22,5 +24,18 @@ public class BasicController {
     @ResponseBody
     String my() {
         return "mypage 입니다.";
+    }
+
+    @GetMapping("/date")
+    @ResponseBody
+    String showDate() {
+        Date today = new Date();
+        System.out.println(today);
+        SimpleDateFormat today_str = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("today_str" + today_str);
+
+        String str_today = today_str.format(today);
+        System.out.println("str >>" +  str_today);
+        return str_today;
     }
 }
